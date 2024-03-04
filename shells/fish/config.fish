a647f889b7f698ef67a3962784f47dd29cc4dedc
fuzoh <info@bastiennicoud.ch>
2024-03-04 11:44:44 +0100

    feat(sway): update config for void

--- shells/fish/config.fish
+++ shells/fish/config.fish
@@ -24,5 +24,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
     zoxide init fish | source
+    set fish_greeting
+    cd
 end
 
