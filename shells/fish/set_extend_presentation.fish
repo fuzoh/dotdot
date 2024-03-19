37e3c53557c02312176952b1be5410bf32867837
fuzoh <info@bastiennicoud.ch>
2024-03-19 14:09:29 +0100

    feat(hypr): monitors and shortcuts updates

--- shells/fish/set_extend_presentation.fish
+++ shells/fish/set_extend_presentation.fish
@@ -1,5 +1,5 @@
 function set_extend_presentation
     hyprctl keyword monitor eDP-1,1920x1200,0x0,1
-    hyprctl keyword monitor ,preferred,auto,auto
-    hyprctl keyword workspace 9,monitor:DP-9,default:true
+    hyprctl keyword monitor HDMI-A-1,preferred,auto,2
+    hyprctl keyword workspace 9,monitor:HDMI-A-1,default:true
 end
