a7aeec3b327e93bf5b00d63cface064f28741982
fuzoh <info@bastiennicoud.ch>
2024-04-15 07:38:04 +0200

    feat(hypr): monitor functions name update

--- shells/fish/studio_display.fish
+++ shells/fish/studio_display.fish
@@ -1,4 +1,4 @@
-function set_studio_display
+function studio_display
     hyprctl keyword monitor DP-6,disable
     hyprctl keyword monitor DP-5,5120x2880,0x0,2
     hyprctl keyword monitor eDP-1,3840x2400,200x1440,2
