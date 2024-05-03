84e7dddb46e2570cc74fd4082e9db975d7a2f999
fuzoh <info@bastiennicoud.ch>
2024-05-03 13:51:27 +0200

    feat: new display functions

--- shells/fish/studio_display.fish
+++ shells/fish/studio_display.fish
@@ -1,5 +1,5 @@
 function studio_display
-    hyprctl keyword monitor DP-6,disable
-    hyprctl keyword monitor DP-5,5120x2880,0x0,2
+    hyprctl keyword monitor DP-5,disable
+    hyprctl keyword monitor DP-6,5120x2880,0x0,2
     hyprctl keyword monitor eDP-1,3840x2400,200x1440,2
 end
