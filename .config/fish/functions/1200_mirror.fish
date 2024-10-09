f6cbb922328a8ea56182ced91465ba96ca62419f
fuzoh <info@bastiennicoud.ch>
2024-10-09 10:29:40 +0200

    chore(hypr,fish): clear no more used display commands

--- .config/fish/functions/1200_mirror.fish
+++ .config/fish/functions/1200_mirror.fish
@@ -1,4 +1,4 @@
 function 1200_mirror
     hyprctl keyword monitor eDP-1,1920x1200,0x0,1
-    hyprctl keyword monitor HDMI-A-1,1920x1200,auto,auto,mirror,eDP-1
+    hyprctl keyword monitor HDMI-A-1,1920x1200,0x0,1,mirror,eDP-1
 end
--- .config/fish/functions/set_jt3_display.fish
+++
@@ -1,4 +0,0 @@
-function set_jt3_display
-    hyprctl keyword monitor DP-10,2560x1440,0x0,1
-    hyprctl keyword monitor eDP-1,1920x1200,280x1440,1
-end
