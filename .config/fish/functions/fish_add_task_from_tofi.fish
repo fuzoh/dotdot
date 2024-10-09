35df1b6a6a3d2aa968b29f836092e8f0edad44b0
fuzoh <info@bastiennicoud.ch>
2024-10-09 10:52:55 +0200

    fix(hypr,scripts): fish scripts cannot be invoked directly from hypr, use standard bash script

--- .config/fish/functions/fish_add_task_from_tofi.fish
+++
@@ -1,5 +0,0 @@
-function fish_add_task_from_tofi
-
-    cat /dev/null | tofi --prompt-text "Task : " | taskwarrior_add_from_stdin
-
-end
--- .config/fish/functions/fish_open_terminal_with_zoxide.fish
+++
@@ -1,5 +0,0 @@
-function fish_open_terminal_with_zoxide
-
-    cat /dev/null | tofi --prompt-text "Goto : " | open_terminal_in_folder
-
-end
