e149a7c5491c0c50f31421844395315053a522be
fuzoh <info@bastiennicoud.ch>
2024-10-21 15:29:40 +0200

    fix(hypr,monitors): add default rules for displays

--- .config/fish/functions/3840_mirror.fish
+++ .config/fish/functions/3840_mirror.fish
@@ -1,4 +1,4 @@
-function 1080_mirror
+function 3840_mirror
     hyprctl keyword monitor HDMI-A-1,3840x2160,0x0,3
-    hyprctl keyword monitor eDP-1,1920x1080,0x0,auto,mirror,HDMI-A-1
+    hyprctl keyword monitor eDP-1,1920x1080,0x0,1,mirror,HDMI-A-1
 end
--- .config/fish/functions/reset_display.fish
+++ .config/fish/functions/reset_display.fish
@@ -1,5 +1,5 @@
 function reset_display
-    hyprctl keyword monitor eDP-1,prefered,0x0,2
+    hyprctl keyword monitor eDP-1,3840x2400,0x0,2
     hyprctl keyword monitor HDMI-A-1,prefered,auto,auto
     hyprctl keyword monitor DP-8,prefered,auto,auto
     hyprctl keyword monitor DP-9,prefered,auto,auto
