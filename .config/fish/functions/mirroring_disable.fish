058ec7273268a8fe61eb6dd683020245d27cf09e
fuzoh <info@bastiennicoud.ch>
2024-10-28 18:40:08 +0100

    feat(btop): add rosé pine temes

---
+++ .config/fish/functions/mirroring_disable.fish
@@ -0,0 +1,6 @@
+function mirroring_disable
+    hyprctl keyword monitor HDMI-A-1, preferred, auto-right, 2
+    hyprctl keyword monitor desc:Samsung Display Corp. 0x4165, prefered, auto, 2
+    hyprctl keyword monitor desc:Samsung Display Corp. 0x4165, disable
+    hyprctl keyword monitor desc:Samsung Display Corp. 0x4165, prefered, auto, 2
+end
