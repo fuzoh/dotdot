958de5ab8830d8b7c42607d44087d53f156647f8
fuzoh <info@bastiennicoud.ch>
2025-03-05 12:31:59 +0100

    feat(fish): update record setup function to only DP-1 display

--- .config/fish/functions/record_mode.fish
+++ .config/fish/functions/record_mode.fish
@@ -1,7 +1,5 @@
 function record_mode
     # Sets the external monitor for recording
     # I use a 3x scaling to create sharp videos in 4k that are nice to watch on mobile
-    hyprctl keyword monitor DP-1,3840x2160,0x0,3
-    # The internal screen stay to 2x scaling to display a prompt or other "backstage" informations (not recorded)
-    hyprctl keyword monitor eDP-1,3840x2400,0x720,2
+    hyprctl keyword monitor DP-1,3840x2160,auto-up,3
 end
--- .config/fish/functions/reset_display.fish
+++ .config/fish/functions/reset_display.fish
@@ -1,6 +1,3 @@
 function reset_display
-    hyprctl keyword monitor eDP-1,3840x2400,0x0,2
-    hyprctl keyword monitor HDMI-A-1,prefered,auto,auto
-    hyprctl keyword monitor DP-8,prefered,auto,auto
-    hyprctl keyword monitor DP-9,prefered,auto,auto
+    hyprctl keyword monitor DP-1,prefered,auto-up,2
 end
