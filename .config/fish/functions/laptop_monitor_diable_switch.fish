a18b9925ef8ace69bf277a2248809f7fd5cd92e2
fuzoh <info@bastiennicoud.ch>
2025-04-28 16:42:21 +0200

    feat(hypr): function to move all windows

--- .config/fish/functions/laptop_monitor_diable_switch.fish
+++ .config/fish/functions/laptop_monitor_diable_switch.fish
@@ -2,7 +2,7 @@ function laptop_monitor_diable_switch
     set -l monitor_status (hyprctl monitors all -j | jq '.[0].disabled')
     switch $monitor_status
         case true
-            hyprctl keyword monitor desc:Samsung Display Corp. 0x4165, prefered, auto, 2
+            hyprctl keyword monitor eDP-1, prefered, auto-down, 2
         case false
             hyprctl keyword monitor eDP-1, disabled
     end
---
+++ .config/fish/functions/move_all_workspaces_to_current.fish
@@ -0,0 +1,11 @@
+function move_all_workspaces_to_current
+    hyprctl dispatch moveworkspacetomonitor 1 current
+    hyprctl dispatch moveworkspacetomonitor 2 current
+    hyprctl dispatch moveworkspacetomonitor 3 current
+    hyprctl dispatch moveworkspacetomonitor 4 current
+    hyprctl dispatch moveworkspacetomonitor 5 current
+    hyprctl dispatch moveworkspacetomonitor 6 current
+    hyprctl dispatch moveworkspacetomonitor 7 current
+    hyprctl dispatch moveworkspacetomonitor 8 current
+    hyprctl dispatch moveworkspacetomonitor 9 current
+end
