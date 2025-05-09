966cd9ad13bbd53c25b47f98d4b3e5965fd356bf
fuzoh <info@bastiennicoud.ch>
2025-05-09 07:45:55 +0200

    feat(rec): update function to allow display selection

--- .config/fish/functions/record_mode.fish
+++ .config/fish/functions/record_mode.fish
@@ -1,5 +1,7 @@
 function record_mode
     # Sets the external monitor for recording
     # I use a 3x scaling to create sharp videos in 4k that are nice to watch on mobile
-    hyprctl keyword monitor DP-1,3840x2160,auto-up,3
+    set display (hyprctl monitors all -j | jq -r 'reverse | .[] | "\(.name)"' | tofi)
+
+    hyprctl keyword monitor $display,3840x2160,auto-up,3
 end
