53961294fdb635c110988a1aea0b7e99aee084ae
fuzoh <info@bastiennicoud.ch>
2025-05-23 17:34:13 +0200

    feat(hypr): fix mirroring script

--- .config/fish/functions/3840_mirror.fish
+++ .config/fish/functions/3840_mirror.fish
@@ -1,4 +1,4 @@
 function 3840_mirror
     hyprctl keyword monitor HDMI-A-1,3840x2160,0x0,3
-    hyprctl keyword monitor eDP-1,1920x1080,0x0,1,mirror,HDMI-A-1
+    hyprctl keyword monitor eDP-1, preferred, auto, 1, mirror, HDMI-A-1
 end
