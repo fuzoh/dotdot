9bb0641186d2093b93ba93107e61616b6b319805
fuzoh <info@bastiennicoud.ch>
2025-09-04 13:47:48 +0200

    feat(hypr): add reset of mirrored screen

--- .config/fish/functions/reset_display.fish
+++ .config/fish/functions/reset_display.fish
@@ -1,3 +1,4 @@
 function reset_display
     hyprctl keyword monitor DP-1,prefered,auto-up,2
+    hyprctl keyword monitor desc:Samsung Electric Company SAMSUNG 0x01000E00,3840x2160@60, auto-right, 3
 end
