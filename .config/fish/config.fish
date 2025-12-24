183d512a4778d107d8e13f496ecbde7ca5996674
fuzoh <info@bastiennicoud.ch>
2025-12-24 12:05:44 +0100

    chore: fish formatting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -98,7 +98,7 @@ end
 # pnpm
 set -gx PNPM_HOME "/home/bastien/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
-    set -gx PATH "$PNPM_HOME" $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
 
